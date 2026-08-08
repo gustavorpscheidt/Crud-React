4bad91926a21be2336ca2b856538d5737758fc82
Gustavo Rückl Pscheidt <gustavorpscheidt@gmail.com>
2026-08-08 19:03:56 -0300

    commit inicial

---
+++ src/assets/components/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import '../css/App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div id="app">
+      <div className="form_holder">
+        <form action="" className="form_login"></form>
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
